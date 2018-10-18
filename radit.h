8fb402cf22cc44cd3310a077d1e9b8df5e7f4c79
David Kimura <dkimura@pivotal.io>
2018-10-18 12:41:19 -0700

    Add basic function prototypes

--- radit.h
+++ radit.h
@@ -49,4 +49,10 @@ int radit_init(struct radit_tree *tree);
 
 int radit_destroy(struct radit_tree *tree);
 
+void *radit_search(const struct radit_tree *tree, const unsigned char *key);
+
+void radit_insert(struct radit_tree *tree, const unsigned char *key, void *value);
+
+void radit_delete(struct radit_tree *tree, const unsigned char *key);
+
 #endif
